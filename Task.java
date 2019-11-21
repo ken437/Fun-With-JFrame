f13c2a2a2c90c058f6a14b4bb1fd219c5e4da0a2
ken437 <41343159+ken437@users.noreply.github.com>
2019-11-21 16:56:13 -0500

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,12 @@
+package jframestuff;
+
+/**
+ * Represents the different tasks that the program can
+ * complete
+ * 
+ * @author ken437
+ * @version 11/13/2019
+ */
+public enum Task {
+    GENERATE_RANDOM_LINES, CREATE_NESTED_SQUARES_FRACTAL, CREATE_TREE_FRACTAL
+}
